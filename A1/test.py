aa1134146e61929e6ba20d0532bfdbc5dc96e3a8
Fromero8706 <felipe.romero@students.iaac.net>
2021-04-20 19:59:24 +1000

    Update test.py

--- A1/test.py
+++ A1/test.py
@@ -2,13 +2,15 @@ import Rhino.Geometry as rg
 
 grid = []
 
-for i in range(10):
-    for j in range(10):
+for i in range(2):
+    for j in range(12):
         grid.append(rg.Point3d(i,j,0))
 
 
 a = grid
 
 line = rg.Line(grid[0], grid[18])
+
+
 
 b = line.Length
